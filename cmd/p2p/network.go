e0747c129f592d2c97f053ca416d7d6458d2ebfa
Trung Nguyen <trung.nguyen01@sap.com>
2024-06-03 01:44:08 +0200

    add network logic to handle message between peer

---
+++ cmd/p2p/network.go
@@ -0,0 +1,112 @@
+package p2p
+
+import (
+	"github.com/golang/protobuf/proto"
+	"log"
+	"net"
+)
+
+type GossipMessage struct {
+	Type    string
+	From    string
+	Payload []byte
+}
+
+func (msg *GossipMessage) Reset() {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (msg *GossipMessage) String() string {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (msg *GossipMessage) ProtoMessage() {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (msg *GossipMessage) Serialize() ([]byte, error) {
+	return proto.Marshal(msg)
+}
+
+func Deserialize(data []byte) (*GossipMessage, error) {
+	var msg GossipMessage
+	err := proto.Unmarshal(data, &msg)
+	if err != nil {
+		return nil, err
+	}
+	return &msg, nil
+}
+
+// SendMessage sends a message to a peer
+func SendMessage(address string, msg *GossipMessage) error {
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		return err
+	}
+
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+		}
+	}(conn)
+
+	data, err := msg.Serialize()
+	if err != nil {
+		return err
+	}
+
+	_, err = conn.Write(data)
+	return err
+}
+
+// p2pAddress instead of + port
+// ListenForMessages listens for incoming messages from peers
+func ListenForMessages(port string, msgHandler func(*GossipMessage)) {
+	ln, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("Failed to start listener: %v", err)
+	}
+	defer func(ln net.Listener) {
+		err := ln.Close()
+		if err != nil {
+			log.Fatalf("Error closing TCP server: %v", err)
+		}
+	}(ln)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println("Failed to accept connection:", err)
+			continue
+		}
+
+		go handleConnection(conn, msgHandler)
+	}
+}
+
+func handleConnection(conn net.Conn, msgHandler func(*GossipMessage)) {
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+
+		}
+	}(conn)
+
+	buf := make([]byte, 4096)
+	n, err := conn.Read(buf)
+	if err != nil {
+		log.Println("Failed to read from connection:", err)
+		return
+	}
+
+	msg, err := Deserialize(buf[:n])
+	if err != nil {
+		log.Println("Failed to deserialize message:", err)
+		return
+	}
+
+	msgHandler(msg)
+}
