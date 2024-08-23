2e6cd863ddf57c672c75b2e9b69024e8b2526b04
Trung Nguyen <trung.nguyen01@sap.com>
2024-08-23 22:59:09 +0200

    implement heartbeat message from gossipnode to bootstrapper

--- cmd/p2p/server.go
+++ cmd/p2p/server.go
@@ -80,6 +80,11 @@ func (node *GossipNode) Start() {
 		node.periodicBootstrapping()
 	}()
 
+	go func() {
+		defer wg.Done()
+		node.sendHeartbeat()
+	}()
+
 	wg.Wait()
 }
 
@@ -215,6 +220,7 @@ func (node *GossipNode) handleGossipMessage(msg *pb.GossipMessage) {
 		logger.Debug("Receiving PeerAnnounce : New Peer is Joining")
 	case int32(enum.PeerLeave):
 		logger.Debug("Receiving PeerLeave : A Peer is leaving")
+		// these 2 might not relevant to be handled as gossip message
 	case int32(enum.PeerListRequest):
 		logger.Debug("Receiving PeerListRequest: A Peer is ")
 	case int32(enum.PeerListResponse):
